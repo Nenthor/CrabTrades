1842081b4abc0f9aea9c3e1253a3e27e343292ff
hm-erusch <e.rusch@hm.edu>
2024-02-10 14:09:35 +0100

    got an about page routed, and a simple error page done

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <h1>Welcome to SvelteKit</h1>
 
 <h2>SAMPLE TEXT</h2>
+<a href="/about">About my site</a>
 
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,13 @@
+<h1>About us</h1>
+
+<p>Sample text1</p>
+
+<p>Sample text2</p>
+
+<p>Sample text3</p>
+
+<p>Sample text4</p>
+
+<p>Sample text5</p>
+
+<p>Sample text6</p>
