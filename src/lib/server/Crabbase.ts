4f6987de4f61ead2eed3aecfe847c69614ab654e
hm-erusch <e.rusch@hm.edu>
2024-02-22 20:30:28 +0100

    problems

--- src/lib/server/Crabbase.ts
+++ src/lib/server/Crabbase.ts
@@ -1,12 +1,32 @@
 import { crabbase } from './FirestoreApp';
 
+/*
 export async function getTest() {
   console.log('Getting test');
   const docRef = crabbase.collection('users').doc('alovelaceaiwgudakwzgd');
 
   await docRef.set({
-    first: 'Ada',
-    last: 'Lovelace',
-    born: 1815,
+    first: 'john',
+    last: 'johnson',
+    born: 22222,
+
+  
   });
+
+}
+*/
+
+export async function writeDB(a: number, b: number, c: string) {
+  //if(typeof a == typeof(number) && typeof b == typeof(number) && typeof c == typeof(string)){
+  console.log('execute writeDB');
+  const docRef = crabbase.collection('DataStuff').doc('BigData');
+
+  await docRef.set({
+    //need to change to variables
+    Aa: a,
+    Ba: b,
+    Ca: c,
+  });
+
+  //}
 }
