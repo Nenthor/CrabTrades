e365ceba7a5f57506fea5eb87a9d7a28ee890f1d
Nentwix <94794597+Nentwix@users.noreply.github.com>
2024-02-29 13:02:31 +0100

    Added mobile layout to homepage charts

--- src/lib/components/HomepageChart.svelte
+++ src/lib/components/HomepageChart.svelte
@@ -3,6 +3,7 @@
   import SimpleChart from './SimpleChart.svelte';
 
   const DURATION = 0.75; // s
+  const DURATION_MOBILE = 0.5; // s
 
   let chartProps1: ChartProps = {
     xLabels: [
@@ -43,8 +44,30 @@
   function onChartClick(index: number) {
     if (index === 1 || isAnimating) return;
     isAnimating = true;
+    const isMobile = window.screen.width <= 1700;
+    const timeout = isMobile ? DURATION_MOBILE * 1000 : DURATION * 1000;
 
     // Animate the charts
+    isMobile ? animateMobileCharts(index) : animateCharts(index);
+
+    setTimeout(() => {
+      // Clear the animation classes
+      isMobile ? clearAnimationClassesMobile(index) : clearAnimationClasses(index);
+
+      // Update the charts array
+      if (index === 0) {
+        const first = charts.shift();
+        if (first) charts.push(first);
+      } else if (index === 2) {
+        const last = charts.pop();
+        if (last) charts.unshift(last);
+      }
+      charts = charts;
+      isAnimating = false;
+    }, timeout);
+  }
+
+  function animateCharts(index: number) {
     const chart0 = document.getElementById('chart0');
     const chart1 = document.getElementById('chart1');
     const chart2 = document.getElementById('chart2');
@@ -64,34 +87,56 @@
       chart2?.classList.add('rightToVoid');
       if (chartLast) chartLast.style.display = 'inline';
     }
+  }
 
-    setTimeout(() => {
-      // Remove the animation classes
-      if (index === 0) {
-        chart0?.classList.remove('leftToVoid');
-        chart1?.classList.remove('middleToLeft');
-        chart2?.classList.remove('rightToMiddle');
-        chart3?.classList.remove('voidToRight');
-        if (chart3) chart3.style.display = 'none';
-      } else if (index === 2) {
-        chartLast?.classList.remove('voidToLeft');
-        chart0?.classList.remove('leftToMiddle');
-        chart1?.classList.remove('middleToRight');
-        chart2?.classList.remove('rightToVoid');
-        if (chartLast) chartLast.style.display = 'none';
-      }
+  function animateMobileCharts(index: number) {
+    const chart0 = document.getElementById('chart0');
+    const chart1 = document.getElementById('chart1');
+    const chart2 = document.getElementById('chart2');
 
-      // Update the charts array
-      if (index === 0) {
-        const first = charts.shift();
-        if (first) charts.push(first);
-      } else if (index === 2) {
-        const last = charts.pop();
-        if (last) charts.unshift(last);
-      }
-      charts = charts;
-      isAnimating = false;
-    }, DURATION * 1000);
+    if (index === 0) {
+      chart1?.classList.add('middleToLeftMobile');
+      chart2?.classList.add('rightToMiddleMobile');
+    } else if (index === 2) {
+      chart0?.classList.add('leftToMiddleMobile');
+      chart1?.classList.add('middleToRightMobile');
+    }
+  }
+
+  function clearAnimationClasses(index: number) {
+    const chart0 = document.getElementById('chart0');
+    const chart1 = document.getElementById('chart1');
+    const chart2 = document.getElementById('chart2');
+    const chart3 = document.getElementById('chart3');
+    const chartLast = document.getElementById(`chart${charts.length - 1}`);
+
+    if (index === 0) {
+      chart0?.classList.remove('leftToVoid');
+      chart1?.classList.remove('middleToLeft');
+      chart2?.classList.remove('rightToMiddle');
+      chart3?.classList.remove('voidToRight');
+      if (chart3) chart3.style.display = 'none';
+    } else if (index === 2) {
+      chartLast?.classList.remove('voidToLeft');
+      chart0?.classList.remove('leftToMiddle');
+      chart1?.classList.remove('middleToRight');
+      chart2?.classList.remove('rightToVoid');
+      if (chartLast) chartLast.style.display = 'none';
+    }
+  }
+
+  function clearAnimationClassesMobile(index: number) {
+    const chart0 = document.getElementById('chart0');
+    const chart1 = document.getElementById('chart1');
+    const chart2 = document.getElementById('chart2');
+
+    if (index === 0) {
+      chart1?.classList.remove('middleToLeftMobile');
+      chart2?.classList.remove('rightToMiddleMobile');
+    } else if (index === 2) {
+      chart0?.classList.remove('leftToMiddleMobile');
+      chart1?.classList.remove('middleToRightMobile');
+    }
   }
 </script>
 
@@ -103,6 +148,14 @@
         <SimpleChart chartProps={chart} />
       </button>
     {/each}
+    <div class="mobile_buttons">
+      <button on:click={() => onChartClick(0)} class="mobile_left">
+        <img src="/images/svg/arrow-mobile.svg" alt="left" />
+      </button>
+      <button on:click={() => onChartClick(2)} class="mobile_right">
+        <img src="/images/svg/arrow-mobile.svg" alt="right" />
+      </button>
+    </div>
   </ul>
 </div>
 
@@ -122,10 +175,11 @@
     justify-content: center;
     align-items: center;
     flex-direction: column;
+    overflow-x: hidden;
   }
 
   .title {
-    font-size: 3rem;
+    font-size: clamp(1rem, 7vw, 3rem);
     color: var(--primary-light);
     background-color: white;
     padding: 75px 50px 20px 50px;
@@ -160,9 +214,69 @@
     left: 50%;
   }
 
+  .mobile_buttons {
+    display: none;
+    position: absolute;
+    align-items: center;
+    justify-content: center;
+    bottom: 0;
+    left: 0;
+    gap: 10px;
+    width: calc(100% - 20px);
+    padding: 0 10px;
+    transform: translate(0, 50%);
+    z-index: 3;
+  }
+
+  .mobile_left,
+  .mobile_right {
+    position: relative;
+    height: 60px;
+    max-width: 345px;
+    flex-grow: 1;
+    flex-basis: 0;
+    border-radius: 10px;
+    background-color: white;
+    box-shadow: 5px 5px 30px 0px #161616cc;
+  }
+
+  .mobile_left:hover,
+  .mobile_right:hover {
+    background-color: var(--primary);
+  }
+
+  .mobile_left > img,
+  .mobile_right > img {
+    width: 100%;
+    height: 100%;
+    transform: scale(1.2);
+    filter: invert(30%) sepia(45%) saturate(4102%) hue-rotate(351deg) brightness(95%) contrast(82%);
+  }
+  .mobile_right > img {
+    transform: scale(1.2) rotate(180deg);
+  }
+
+  .mobile_left:hover > img,
+  .mobile_right:hover > img {
+    filter: invert(1);
+  }
+
   #chart0 {
     display: inline;
-    transform: translate(-50%, -50%) translateX(-730px) perspective(50em) rotateY(-45deg) scale(0.72) translateX(75px);
+    transform: translate(-50%, -50%) translateX(-700px) perspective(50em) rotateY(-45deg) scale(0.74) translateX(75px);
+    z-index: 1;
+  }
+
+  #chart1 {
+    display: inline;
+    transform: translate(-50%, -50%);
+    z-index: 2;
+    cursor: auto;
+  }
+
+  #chart2 {
+    display: inline;
+    transform: translate(-50%, -50%) translateX(700px) perspective(50em) rotateY(45deg) scale(0.74) translateX(-75px);
     z-index: 1;
   }
 
@@ -176,7 +290,7 @@
     position: absolute;
     width: 200px;
     height: 200px;
-    opacity: 0.8;
+    opacity: 0.5;
     top: 50%;
     left: 50%;
     transform: translate(-50%, -50%);
@@ -186,23 +300,35 @@
     background-position: center;
   }
 
-  #chart1 {
-    display: inline;
-    transform: translate(-50%, -50%);
-    z-index: 2;
-    cursor: auto;
-  }
-
-  #chart2 {
-    display: inline;
-    transform: translate(-50%, -50%) translateX(730px) perspective(50em) rotateY(45deg) scale(0.72) translateX(-75px);
-    z-index: 1;
-  }
-
   #chart2::after,
   :global(.voidToRight::after),
   :global(.middleToRight::after) {
     transform: translate(-50%, -50%) rotateY(180deg) !important;
+  }
+
+  @media (max-width: 1700px) {
+    #chart0,
+    #chart2 {
+      display: none;
+    }
+
+    #chart1 {
+      width: min(calc(100vw - 20px), 700px);
+    }
+
+    .mobile_buttons {
+      display: flex;
+    }
+
+    .charts {
+      height: 520px;
+      transform: translateY(-50px);
+    }
+
+    #chart0::after,
+    #chart2::after {
+      content: none;
+    }
   }
 
   :global(.middleToLeft::after),
@@ -252,22 +378,84 @@
     animation: 0.75s ease-in-out 0s 1 reverse forwards running voidToRight;
   }
 
+  :global(.middleToLeftMobile) {
+    animation: 0.5s ease-in-out 0s 1 normal forwards running middleToLeftMobile;
+  }
+
+  :global(.rightToMiddleMobile) {
+    display: inline !important;
+    animation: 0.5s ease-in-out 0s 1 normal forwards running rightToMiddleMobile;
+  }
+
+  :global(.leftToMiddleMobile) {
+    display: inline !important;
+    animation: 0.5s ease-in-out 0s 1 normal forwards running leftToMiddleMobile;
+  }
+
+  :global(.middleToRightMobile) {
+    animation: 0.5s ease-in-out 0s 1 reverse forwards running rightToMiddleMobile;
+  }
+
+  @keyframes rightToMiddleMobile {
+    from {
+      transform: translate(-50%, -50%) translateX(700px);
+      opacity: 0;
+    }
+    to {
+      transform: translate(-50%, -50%);
+      opacity: 1;
+    }
+  }
+
+  @keyframes leftToMiddleMobile {
+    from {
+      transform: translate(-50%, -50%) translateX(-700px);
+      opacity: 0;
+    }
+    to {
+      transform: translate(-50%, -50%);
+      opacity: 1;
+    }
+  }
+
+  @keyframes middleToRightMobile {
+    from {
+      transform: translate(-50%, -50%);
+      opacity: 1;
+    }
+    to {
+      transform: translate(-50%, -50%) translateX(700px);
+      opacity: 0;
+    }
+  }
+
+  @keyframes middleToLeftMobile {
+    from {
+      transform: translate(-50%, -50%);
+      opacity: 1;
+    }
+    to {
+      transform: translate(-50%, -50%) translateX(-700px);
+      opacity: 0;
+    }
+  }
+
   @keyframes arrowFadeIn {
     from {
       opacity: 0;
     }
     to {
-      opacity: 0.8;
+      opacity: 0.5;
     }
   }
 
   @keyframes leftToVoid {
     from {
-      transform: translate(-50%, -50%) translateX(-730px) perspective(50em) rotateY(-45deg) scale(0.72) translateX(75px);
+      transform: translate(-50%, -50%) translateX(-700px) perspective(50em) rotateY(-45deg) scale(0.74) translateX(75px);
       opacity: 1;
     }
     to {
-      transform: translate(-50%, -50%) translateX(-830px) perspective(50em) rotateY(-90deg) scale(0.72) translateX(75px);
+      transform: translate(-50%, -50%) translateX(-800px) perspective(50em) rotateY(-90deg) scale(0.74) translateX(75px);
       opacity: 0;
     }
   }
@@ -277,13 +465,13 @@
       transform: translate(-50%, -50%);
     }
     to {
-      transform: translate(-50%, -50%) translateX(-730px) perspective(50em) rotateY(-45deg) scale(0.72) translateX(75px);
+      transform: translate(-50%, -50%) translateX(-700px) perspective(50em) rotateY(-45deg) scale(0.74) translateX(75px);
     }
   }
 
   @keyframes rightToMiddle {
     from {
-      transform: translate(-50%, -50%) translateX(730px) perspective(50em) rotateY(45deg) scale(0.72) translateX(-75px);
+      transform: translate(-50%, -50%) translateX(700px) perspective(50em) rotateY(45deg) scale(0.74) translateX(-75px);
     }
     to {
       transform: translate(-50%, -50%);
@@ -292,11 +480,11 @@
 
   @keyframes voidToRight {
     from {
-      transform: translate(-50%, -50%) translateX(830px) perspective(50em) rotateY(90deg) scale(0.72) translateX(-75px);
+      transform: translate(-50%, -50%) translateX(800px) perspective(50em) rotateY(90deg) scale(0.74) translateX(-75px);
       opacity: 0;
     }
     to {
-      transform: translate(-50%, -50%) translateX(730px) perspective(50em) rotateY(45deg) scale(0.72) translateX(-75px);
+      transform: translate(-50%, -50%) translateX(700px) perspective(50em) rotateY(45deg) scale(0.74) translateX(-75px);
       opacity: 1;
     }
   }
