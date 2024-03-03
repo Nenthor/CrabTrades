e1dbf34ed1ed36d867c4729c254da71995936f64
bielov <81532412+vbielov@users.noreply.github.com>
2024-03-03 20:44:21 +0100

    ai integration (#11)

---
+++ trader/run.sh
@@ -0,0 +1,31 @@
+# Guide Install TensorFlow on Debian 11: https://kifarunix.com/install-tensorflow-on-debian/
+
+VENV_NAME="tensorflow_env"
+
+python3 -V # TensorFlow requires Python 3.7+
+
+# Check if the virtual environment exists
+if [ ! -d "$VENV_NAME" ]; then
+    # Create the virtual environment if it doesn't exist
+    python3 -m venv "$VENV_NAME"
+fi
+
+# Activate the virtual environment
+source "$VENV_NAME/bin/activate"
+
+pip --version # pip version 19.0 or higher is required
+
+# Check and install necessary dependencies
+while read -r line || [[ -n "$line" ]]; do
+    package=$(echo "$line" | cut -d '=' -f 1)  # Extract package name
+    if ! pip show "$package" &> /dev/null; then
+        # Install the package if it's not installed
+        pip install "$line"
+    fi
+done < ./requirements.txt
+
+# Run script
+python main.py
+
+# Deactivate the virtual environment
+deactivate
