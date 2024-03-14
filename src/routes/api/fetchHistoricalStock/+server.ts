fd03eb264595848d220967a74eb4cf50f8263fba
Nentwix <94794597+Nentwix@users.noreply.github.com>
2024-03-14 14:58:16 +0100

    Added CSV support to fetchHistoricalStock

--- src/routes/api/fetchHistoricalStock/+server.ts
+++ src/routes/api/fetchHistoricalStock/+server.ts
@@ -1,5 +1,5 @@
 import { ALPACA_KEY, ALPACA_SECRET } from '$env/static/private';
-import { getHistoricalStockDataAwait } from '$lib/Alpaca';
+import { getHistoricalStockDataAwait, toStockFileString } from '$lib/Alpaca';
 import type { RequestHandler } from '@sveltejs/kit';
 
 export const POST = (async ({ request }) => {
@@ -7,12 +7,18 @@ export const POST = (async ({ request }) => {
   const startDate = request.headers.get('startdate') || '';
   const endDate = request.headers.get('enddate') || '';
   const timeframe: any = request.headers.get('timeframe') || '';
+  const type = request.headers.get('type') || 'json'; // optional, default to json
 
-  if (!symbol || !startDate || !endDate || !timeframe) {
+  if (!symbol || !startDate || !endDate || !timeframe || !type) {
     return new Response('Missing required parameters', { status: 400 });
   }
 
-  const data = await getHistoricalStockDataAwait(symbol, new Date(startDate), new Date(endDate), timeframe, ALPACA_KEY, ALPACA_SECRET);
+  if (type !== 'csv' && type !== 'json') {
+    return new Response('Invalid type', { status: 400 });
+  }
 
-  return new Response(JSON.stringify(data), { status: 200, headers: { 'Content-Type': 'application/json' } });
+  const data = await getHistoricalStockDataAwait(symbol, new Date(startDate), new Date(endDate), timeframe, ALPACA_KEY, ALPACA_SECRET);
+  const body = type === 'json' ? JSON.stringify(data) : toStockFileString(data);
+
+  return new Response(body, { status: 200 });
 }) satisfies RequestHandler;
